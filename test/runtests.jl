9c9fbf2577704c9263b507efda2999657ad37252
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 01:13:44 +0000

    Snapshot of branch master taken on 2019-04-07T21:13:43.795-04:00 from "https://github.com/UnofficialJuliaMirror/PSFWrite.jl-1a094a50-539a-5eba-b3bf-75406d1a4751"

---
+++ test/runtests.jl
@@ -0,0 +1,50 @@
+#Test code
+#-------------------------------------------------------------------------------
+
+using PSFWrite
+using LibPSF #To test validity
+using Test
+
+
+#==Input data
+===============================================================================#
+sampledata(filename::String) = joinpath(LibPSF.rootpath, "core/data", filename)
+sepline = "---------------------------------------------------------------------"
+printsep() = println(sepline)
+
+filepath = "testfile.psf"
+
+freq = 1e9
+#t = collect(0:.1e-9:10e-9)
+t = collect(0:.01e-9:10e-9)
+y1 = sin.(t*(2pi*freq))
+y2 = cos.(t*(2pi*freq))
+
+
+#==Tests
+===============================================================================#
+@testset "Writing time-domain data to PSF file" begin
+	@info("Writing out data with PSFWrite...")
+	data = PSFWrite.dataset(t, "time")
+	push!(data, y1, "y1")
+	push!(data, y2, "y2")
+
+	PSFWrite._open(filepath) do writer
+		write(writer, data)
+	end
+
+	@info("Reading data back in with LibPSF...")
+	reader = LibPSF._open(filepath)
+	#display(reader.props)
+
+	_t = readsweep(reader)
+	#_t[3] = 2 * _t[3] #Inject error
+	@test _t == t
+	_y1 = read(reader, "y1")
+	@test _y1 == y1
+	_y2 = read(reader, "y2")
+	@test _y2 == y2
+	close(reader)
+end
+
+:Test_Complete
